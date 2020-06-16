a7b63d89adc14c7f1c86e98ad5abd2ec7046e081
Shon89-DevOps <62050226+Shon89-DevOps@users.noreply.github.com>
2020-06-16 14:27:47 +0900

    Update high-number.py

--- sort/high-number.py
+++ sort/high-number.py
@@ -1,7 +1,7 @@
 # https://programmers.co.kr/learn/courses/30/lessons/42746?language=python3
 def solution(numbers):
 
-    # 맨 처음숫자가 가장 큰 순서대로 나열하면됩니다.
+    # map 을 통해서 str로 형변환
     numbers = list(map(str,numbers))
     # 리시트를 정렬하여 비교함.
     answer = sorted(numbers,key=lambda x : x*3 , reverse=True)
